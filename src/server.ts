c966ac157ecb91d803b51f9d9034347259b71927
wangkailun <wangkailun@lidig.com>
2023-02-28 10:23:52 +0800

    创建

---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import { TuyaContext } from '@tuya/tuya-connector-nodejs';
+const app = new Koa();
+const router = new Router();
+
+const context = new TuyaContext({
+  baseUrl: 'https://openapi.tuyacn.com',
+  accessKey: "374rek94rujd4vmfnv4k",
+  secretKey: "2bc626355a57453b84a7f41ef107404e",
+});
+
+const main = async () => {
+ //定义设备id
+  const device_id = "6c61fcf54cefbdbc5akbez";
+//下发指令
+  const commands = await context.request({
+    path: `/v1.1/iot-03/devices/${device_id}`,
+    method: 'GET',
+    // body: {
+    //   "commands":[{"code":"switch_led","value":true}]
+    // }
+  });
+  if(!commands.success) {
+    new Error();
+  }
+  console.log("执行结果:",commands);
+  return commands;
+};
+router.get('/cc', async (ctx) => {
+  const res = await main();
+    ctx.body = res;
+});
+
+app.use(router.routes());
+
+app.listen(3001);
+
+console.log('Server running on port 3001');
